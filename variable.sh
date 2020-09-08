3e11f01d056dc69525c01b944fc62bde841a0867
samuel <samybimbo@gmail.com>
2020-09-07 21:44:56 -0400

    mew file

---
+++ variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Description: Variable Study
+# Author:Samuel
+#Date: 03/2020
+
+
+NAME= Alicia
+CITY= Paris
+echo "$NAME is a good worker"
+echo" the manager loves $NAME a lot and others are jealous"
+echo " Other people thing that $NAME likes to show off"
+echo" Samuel's best city is $CITY"
+~
+
